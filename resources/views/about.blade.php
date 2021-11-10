11ec42605008d665e021440fee87b0483ff163f4
sahatilfat <sahatilfat@gmail.com>
2021-11-10 13:10:15 +0700

    Blade Template Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WPU Blog | About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <h1>Halaman About</h1>
-    <h3> @php
-        echo $name;
-    @endphp </h3>
-    <p>@php
-        echo $email
-    @endphp</p>
-    <img  src="img/@php echo $image;@endphp" alt="M Tafli Tahas" style="width: 10vw">
-</body>
-</html>
+@extends('layouts.main')
+
+
+@section('container')
+<h1>Halaman About</h1>
+<h3>{{ $name }}</h3>
+<p>{{ $email }}</p>
+<img src="img/{{ $image }}" alt="{{ $name }}" style="width: 10vw">
+@endsection
+   
+ 
--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,72 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'title' => 'Home'
+    ]);
 });
 Route::get('/about', function () {
     return view('about', [
+        'title' => 'About',
         'name' => 'M Tafli Tahas',
         'email' => 'sahatilfat@gmail.com',
         'image' => 'image.png'
     ]);
 });
+
+
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => " M Tafli Tahas",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus ipsam corporis eos sunt facere ipsa! Omnis enim aspernatur, dolore voluptas nobis dolorum alias dolores quidem reiciendis suscipit cumque distinctio labore dicta ipsum, harum asperiores optio voluptatem nam aperiam fugit sint. A commodi iure recusandae ullam porro eum quidem ab temporibus voluptatibus magni quibusdam rem in nulla mollitia doloribus, vero nisi praesentium officia, exercitationem perspiciatis itaque, earum voluptate facilis! Dolorem facilis ipsum natus sit fugiat assumenda optio esse. Et, perferendis?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => " Sahat Ilfat",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus ipsam corporis eos sunt facere ipsa! Omnis enim aspernatur, dolore voluptas nobis dolorum alias dolores quidem reiciendis suscipit cumque distinctio labore dicta ipsum, harum asperiores optio voluptatem nam aperiam fugit sint. A commodi iure recusandae ullam porro eum quidem ab temporibus voluptatibus magni quibusdam rem in nulla mollitia doloribus, vero nisi praesentium officia, exercitationem perspiciatis itaque, earum voluptate facilis! Dolorem facilis ipsum natus sit fugiat assumenda optio esse. Et, perferendis?"
+        ],
+
+    ];
+    return view('posts', [
+        'title' => 'Posts',
+        'posts' => $blog_posts,
+    ]);
+});
+
+// halaman single post
+Route::get('posts/{slug}', function ($slug) {
+
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => " M Tafli Tahas",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus ipsam corporis eos sunt facere ipsa! Omnis enim aspernatur, dolore voluptas nobis dolorum alias dolores quidem reiciendis suscipit cumque distinctio labore dicta ipsum, harum asperiores optio voluptatem nam aperiam fugit sint. A commodi iure recusandae ullam porro eum quidem ab temporibus voluptatibus magni quibusdam rem in nulla mollitia doloribus, vero nisi praesentium officia, exercitationem perspiciatis itaque, earum voluptate facilis! Dolorem facilis ipsum natus sit fugiat assumenda optio esse. Et, perferendis?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => " Sahat Ilfat",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus ipsam corporis eos sunt facere ipsa! Omnis enim aspernatur, dolore voluptas nobis dolorum alias dolores quidem reiciendis suscipit cumque distinctio labore dicta ipsum, harum asperiores optio voluptatem nam aperiam fugit sint. A commodi iure recusandae ullam porro eum quidem ab temporibus voluptatibus magni quibusdam rem in nulla mollitia doloribus, vero nisi praesentium officia, exercitationem perspiciatis itaque, earum voluptate facilis! Dolorem facilis ipsum natus sit fugiat assumenda optio esse. Et, perferendis?"
+        ],
+
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post['slug'] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        'title' => "Single Post",
+        'post' => $new_post,
+    ]);
 });
