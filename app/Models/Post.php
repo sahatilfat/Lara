47b1372478507b1cd89e96eeafb9621248ae3c9e
sahatilfat <sahatilfat@gmail.com>
2021-11-11 13:58:46 +0700

    Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,33 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => " M Tafli Tahas",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus ipsam corporis eos sunt facere ipsa! Omnis enim aspernatur, dolore voluptas nobis dolorum alias dolores quidem reiciendis suscipit cumque distinctio labore dicta ipsum, harum asperiores optio voluptatem nam aperiam fugit sint. A commodi iure recusandae ullam porro eum quidem ab temporibus voluptatibus magni quibusdam rem in nulla mollitia doloribus, vero nisi praesentium officia, exercitationem perspiciatis itaque, earum voluptate facilis! Dolorem facilis ipsum natus sit fugiat assumenda optio esse. Et, perferendis?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => " Sahat Ilfat",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora natus ipsam corporis eos sunt facere ipsa! Omnis enim aspernatur, dolore voluptas nobis dolorum alias dolores quidem reiciendis suscipit cumque distinctio labore dicta ipsum, harum asperiores optio voluptatem nam aperiam fugit sint. A commodi iure recusandae ullam porro eum quidem ab temporibus voluptatibus magni quibusdam rem in nulla mollitia doloribus, vero nisi praesentium officia, exercitationem perspiciatis itaque, earum voluptate facilis! Dolorem facilis ipsum natus sit fugiat assumenda optio esse. Et, perferendis?"
-        ],
+    use HasFactory;
 
-    ];
-
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    protected $fillable = ['title', 'slug', 'excerpt', 'body'];
+    protected $quarded = ['id'];
 }
