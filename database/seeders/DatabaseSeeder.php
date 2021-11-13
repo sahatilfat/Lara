7f11c17736cc9fe4a3df3d1e7afd8bddd9080df2
sahatilfat <sahatilfat@gmail.com>
2021-11-13 11:36:45 +0700

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,57 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'M Tafli Tahas',
+            'email' => 'sahatilfat@gmail.com',
+            'password' => bcrypt('12345'),
+        ]);
+        User::create([
+            'name' => 'Bahari Lubis',
+            'email' => 'bahari@gmail.com',
+            'password' => bcrypt('12345'),
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, consectetur! Consectetur voluptas officia eveniet, laboriosam totam rem vitae ipsa facilis alias, architecto dolorum veniam. Nostrum quos dolorem accusantium eum incidunt et sequi numquam odio autem',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, consectetur! Consectetur voluptas officia eveniet, laboriosam totam rem vitae ipsa facilis alias, architecto dolorum veniam. Nostrum quos dolorem accusantium eum incidunt et sequi numquam odio autem distinctio blanditiis in mollitia illum quaerat, unde omnis quod corrupti doloremque voluptatem fuga. Voluptatibus, ipsa enim. Dolorem pariatur, ratione ut nisi deserunt minus consequuntur. Accusamus laudantium a, quidem possimus tenetur, adipisci odio quos, facilis laborum quisquam vitae illum tempore dolorem corrupti praesentium aspernatur? Facere incidunt labore consequuntur, autem, eaque ipsa corrupti at ipsum, ut laboriosam deserunt inventore! Accusamus, voluptatem? Esse quos perferendis eum natus. Alias?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, consectetur! Consectetur voluptas officia eveniet, laboriosam totam rem vitae ipsa facilis alias, architecto dolorum veniam. Nostrum quos dolorem accusantium eum incidunt et sequi numquam odio autem',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, consectetur! Consectetur voluptas officia eveniet, laboriosam totam rem vitae ipsa facilis alias, architecto dolorum veniam. Nostrum quos dolorem accusantium eum incidunt et sequi numquam odio autem distinctio blanditiis in mollitia illum quaerat, unde omnis quod corrupti doloremque voluptatem fuga. Voluptatibus, ipsa enim. Dolorem pariatur, ratione ut nisi deserunt minus consequuntur. Accusamus laudantium a, quidem possimus tenetur, adipisci odio quos, facilis laborum quisquam vitae illum tempore dolorem corrupti praesentium aspernatur? Facere incidunt labore consequuntur, autem, eaque ipsa corrupti at ipsum, ut laboriosam deserunt inventore! Accusamus, voluptatem? Esse quos perferendis eum natus. Alias?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, consectetur! Consectetur voluptas officia eveniet, laboriosam totam rem vitae ipsa facilis alias, architecto dolorum veniam. Nostrum quos dolorem accusantium eum incidunt et sequi numquam odio autem',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, consectetur! Consectetur voluptas officia eveniet, laboriosam totam rem vitae ipsa facilis alias, architecto dolorum veniam. Nostrum quos dolorem accusantium eum incidunt et sequi numquam odio autem distinctio blanditiis in mollitia illum quaerat, unde omnis quod corrupti doloremque voluptatem fuga. Voluptatibus, ipsa enim. Dolorem pariatur, ratione ut nisi deserunt minus consequuntur. Accusamus laudantium a, quidem possimus tenetur, adipisci odio quos, facilis laborum quisquam vitae illum tempore dolorem corrupti praesentium aspernatur? Facere incidunt labore consequuntur, autem, eaque ipsa corrupti at ipsum, ut laboriosam deserunt inventore! Accusamus, voluptatem? Esse quos perferendis eum natus. Alias?',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, consectetur! Consectetur voluptas officia eveniet, laboriosam totam rem vitae ipsa facilis alias, architecto dolorum veniam. Nostrum quos dolorem accusantium eum incidunt et sequi numquam odio autem',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate, consectetur! Consectetur voluptas officia eveniet, laboriosam totam rem vitae ipsa facilis alias, architecto dolorum veniam. Nostrum quos dolorem accusantium eum incidunt et sequi numquam odio autem distinctio blanditiis in mollitia illum quaerat, unde omnis quod corrupti doloremque voluptatem fuga. Voluptatibus, ipsa enim. Dolorem pariatur, ratione ut nisi deserunt minus consequuntur. Accusamus laudantium a, quidem possimus tenetur, adipisci odio quos, facilis laborum quisquam vitae illum tempore dolorem corrupti praesentium aspernatur? Facere incidunt labore consequuntur, autem, eaque ipsa corrupti at ipsum, ut laboriosam deserunt inventore! Accusamus, voluptatem? Esse quos perferendis eum natus. Alias?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,11 +5,11 @@
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <p>By. M Tafli Tahas in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="#" style="text-decoration: none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" style="text-decoration: none">{{ $post->category->name }}</a></p>
 
    {!! $post->body !!}
 
-    <a href="/posts">Back to Posts</a>
+    <a href="/posts" class="d-block mt-3">Back to Posts</a>
 @endsection 
 
 
