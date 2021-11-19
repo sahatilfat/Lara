663a5548f9151b360d7c587209759f9412acb5b9
sahatilfat <sahatilfat@gmail.com>
2021-11-19 11:01:20 +0700

    Searching & Pagination

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,7 +10,7 @@
 
             <h1 class="mb-3">{{ $post->title }}</h1>
 
-            <p>By. <a href="/authors/{{ $post->author->username }}" style="text-decoration: none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" style="text-decoration: none">{{ $post->category->name }}</a></p>
+            <p>By. <a href="/posts?author={{ $post->author->username }}" style="text-decoration: none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" style="text-decoration: none">{{ $post->category->name }}</a></p>
 
             <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
 
